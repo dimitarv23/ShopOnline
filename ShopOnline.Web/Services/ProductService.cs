e1630e8bf98d95a79ffb634f706a95c9d86f9bb0
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-10-31 11:57:07 +0200

    Added functionalities

---
+++ ShopOnline.Web/Services/ProductService.cs
@@ -0,0 +1,29 @@
+﻿using ShopOnline.Models.DTOs;
+using ShopOnline.Web.Services.Contracts;
+using System.Net.Http.Json;
+
+namespace ShopOnline.Web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetItems()
+        {
+            try
+            {
+                var products = await _httpClient.GetFromJsonAsync<IEnumerable<ProductDto>>("api/Product");
+                return products;
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
