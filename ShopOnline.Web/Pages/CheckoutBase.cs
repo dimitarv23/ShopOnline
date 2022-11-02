b0c3cb189b30b94c48705543aec49828f55d8dbe
Dimitar Vasilev <captaingetrekt@gmail.com>
2022-11-02 10:29:38 +0200

    Added payment methods

---
+++ ShopOnline.Web/Pages/CheckoutBase.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using ShopOnline.Models.DTOs;
+using ShopOnline.Web.Services.Contracts;
+
+namespace ShopOnline.Web.Pages
+{
+    public class CheckoutBase : ComponentBase
+    {
+        [Inject]
+        public IJSRuntime Js { get; set; }
+
+        protected IEnumerable<CartItemDto> ShoppingCartItems { get; set; }
+
+        protected int TotalQuantity { get; set; }
+
+        protected string PaymentDescription { get; set; }
+
+        protected decimal PaymentAmount { get; set; }
+
+        [Inject]
+        public IShoppingCartService ShoppingCartService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                ShoppingCartItems = await ShoppingCartService.GetItems(HardCoded.UserID);
+
+                if (ShoppingCartItems != null)
+                {
+                    Guid orderGuid = Guid.NewGuid();
+
+                    PaymentAmount = ShoppingCartItems.Sum(p => p.TotalPrice);
+                    TotalQuantity = ShoppingCartItems.Sum(p => p.Quantity);
+                    PaymentDescription = $"O_{HardCoded.UserID}_{orderGuid}";
+                }
+            }
+            catch (Exception)
+            {
+                //Log exception
+                throw;
+            }
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            try
+            {
+                if (firstRender)
+                {
+                    await Js.InvokeVoidAsync("initPayPalButton");
+                }
+            }
+            catch (Exception)
+            {
+                //Log exception
+                throw;
+            }
+        }
+    }
+}
